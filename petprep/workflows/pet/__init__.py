97699ec3ca8fcdcfe21bea19406d8a848dfb1b80
mnoergaard <martin.noergaard@nru.dk>
2022-03-02 14:52:11 -0800

    Updata __init__.py in pet workflow to remove fmri parts

--- petprep/workflows/pet/__init__.py
+++ petprep/workflows/pet/__init__.py
@@ -7,8 +7,6 @@ Pre-processing fMRI - BOLD signal workflows
 
 .. automodule:: fmriprep.workflows.bold.base
 .. automodule:: fmriprep.workflows.bold.hmc
-.. automodule:: fmriprep.workflows.bold.stc
-.. automodule:: fmriprep.workflows.bold.t2s
 .. automodule:: fmriprep.workflows.bold.registration
 .. automodule:: fmriprep.workflows.bold.resampling
 .. automodule:: fmriprep.workflows.bold.confounds
@@ -16,35 +14,29 @@ Pre-processing fMRI - BOLD signal workflows
 
 """
 
-from .base import init_func_preproc_wf
-from .hmc import init_bold_hmc_wf
-from .stc import init_bold_stc_wf
-from .t2s import init_bold_t2s_wf
+from .base import init_pet_preproc_wf
+from .hmc import init_pet_hmc_wf
 from .registration import (
-    init_bold_t1_trans_wf,
-    init_bold_reg_wf,
+    init_pet_t1_trans_wf,
+    init_pet_reg_wf,
 )
 from .resampling import (
-    init_bold_std_trans_wf,
-    init_bold_surf_wf,
-    init_bold_preproc_trans_wf,
+    init_pet_std_trans_wf,
+    init_pet_surf_wf,
+    init_pet_preproc_trans_wf,
 )
 
 from .confounds import (
-    init_bold_confs_wf,
-    init_ica_aroma_wf,
+    init_pet_confs_wf
 )
 
 __all__ = [
-    'init_bold_confs_wf',
-    'init_bold_hmc_wf',
-    'init_bold_std_trans_wf',
-    'init_bold_preproc_trans_wf',
-    'init_bold_reg_wf',
-    'init_bold_stc_wf',
-    'init_bold_surf_wf',
-    'init_bold_t1_trans_wf',
-    'init_bold_t2s_wf',
-    'init_func_preproc_wf',
-    'init_ica_aroma_wf',
+    'init_pet_confs_wf',
+    'init_pet_hmc_wf',
+    'init_pet_std_trans_wf',
+    'init_pet_preproc_trans_wf',
+    'init_pet_reg_wf',
+    'init_pet_surf_wf',
+    'init_pet_t1_trans_wf',
+    'init_pet_preproc_wf'
 ]
