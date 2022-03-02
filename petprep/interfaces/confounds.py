1d4ebbf00d7eb0c556a77584a9b31b6087ac6cf5
mnoergaard <martin.noergaard@nru.dk>
2022-03-02 14:44:32 -0800

    Update confounds.py - removing compcor and ICA aroma

--- petprep/interfaces/confounds.py
+++ petprep/interfaces/confounds.py
@@ -43,141 +43,13 @@ from nipype.interfaces.base import (
 
 LOGGER = logging.getLogger('nipype.interface')
 
-
-class _aCompCorMasksInputSpec(BaseInterfaceInputSpec):
-    in_vfs = InputMultiObject(File(exists=True), desc="Input volume fractions.")
-    is_aseg = traits.Bool(False, usedefault=True,
-                          desc="Whether the input volume fractions come from FS' aseg.")
-    bold_zooms = traits.Tuple(traits.Float, traits.Float, traits.Float, mandatory=True,
-                              desc="BOLD series zooms")
-
-
-class _aCompCorMasksOutputSpec(TraitedSpec):
-    out_masks = OutputMultiObject(File(exists=True),
-                                  desc="CSF, WM and combined masks, respectively")
-
-
-class aCompCorMasks(SimpleInterface):
-    """Generate masks in T1w space for aCompCor."""
-
-    input_spec = _aCompCorMasksInputSpec
-    output_spec = _aCompCorMasksOutputSpec
-
-    def _run_interface(self, runtime):
-        from ..utils.confounds import acompcor_masks
-        self._results["out_masks"] = acompcor_masks(
-            self.inputs.in_vfs,
-            self.inputs.is_aseg,
-            self.inputs.bold_zooms,
-        )
-        return runtime
-
-
-class _FilterDroppedInputSpec(BaseInterfaceInputSpec):
-    in_file = File(exists=True, desc='input CompCor metadata')
-
-
-class _FilterDroppedOutputSpec(TraitedSpec):
-    out_file = File(desc='filtered CompCor metadata')
-
-
-class FilterDropped(SimpleInterface):
-    """Filter dropped components from CompCor metadata files
-
-    Uses the boolean ``retained`` column to identify rows to keep or filter.
-    """
-    input_spec = _FilterDroppedInputSpec
-    output_spec = _FilterDroppedOutputSpec
-
-    def _run_interface(self, runtime):
-        self._results["out_file"] = fname_presuffix(
-            self.inputs.in_file,
-            suffix='_filtered',
-            use_ext=True,
-            newpath=runtime.cwd)
-
-        metadata = pd.read_csv(self.inputs.in_file, sep='\t')
-        metadata[metadata.retained].to_csv(self._results["out_file"], sep='\t', index=False)
-
-        return runtime
-
-
-class _RenameACompCorInputSpec(BaseInterfaceInputSpec):
-    components_file = File(exists=True, desc='input aCompCor components')
-    metadata_file = File(exists=True, desc='input aCompCor metadata')
-
-
-class _RenameACompCorOutputSpec(TraitedSpec):
-    components_file = File(desc='output aCompCor components')
-    metadata_file = File(desc='output aCompCor metadata')
-
-
-class RenameACompCor(SimpleInterface):
-    """Rename ACompCor components based on their masks
-
-    Components from the "CSF" mask are ``c_comp_cor_*``.
-    Components from the "WM" mask are ``w_comp_cor_*``.
-    Components from the "combined" mask are ``a_comp_cor_*``.
-
-    Each set of components is renumbered to start at ``?_comp_cor_00``.
-    """
-    input_spec = _RenameACompCorInputSpec
-    output_spec = _RenameACompCorOutputSpec
-
-    def _run_interface(self, runtime):
-        self._results["components_file"] = fname_presuffix(
-            self.inputs.components_file,
-            suffix='_renamed',
-            use_ext=True,
-            newpath=runtime.cwd)
-        self._results["metadata_file"] = fname_presuffix(
-            self.inputs.metadata_file,
-            suffix='_renamed',
-            use_ext=True,
-            newpath=runtime.cwd)
-
-        components = pd.read_csv(self.inputs.components_file, sep='\t')
-        metadata = pd.read_csv(self.inputs.metadata_file, sep='\t')
-        all_comp_cor = metadata[metadata["retained"]]
-
-        c_comp_cor = all_comp_cor[all_comp_cor["mask"] == "CSF"]
-        w_comp_cor = all_comp_cor[all_comp_cor["mask"] == "WM"]
-        a_comp_cor = all_comp_cor[all_comp_cor["mask"] == "combined"]
-
-        c_orig = c_comp_cor["component"]
-        c_new = [f"c_comp_cor_{i:02d}" for i in range(len(c_orig))]
-
-        w_orig = w_comp_cor["component"]
-        w_new = [f"w_comp_cor_{i:02d}" for i in range(len(w_orig))]
-
-        a_orig = a_comp_cor["component"]
-        a_new = [f"a_comp_cor_{i:02d}" for i in range(len(a_orig))]
-
-        (components.rename(columns=dict(zip(c_orig, c_new)))
-                   .rename(columns=dict(zip(w_orig, w_new)))
-                   .rename(columns=dict(zip(a_orig, a_new)))
-         ).to_csv(self._results["components_file"], sep='\t', index=False)
-
-        metadata.loc[c_comp_cor.index, "component"] = c_new
-        metadata.loc[w_comp_cor.index, "component"] = w_new
-        metadata.loc[a_comp_cor.index, "component"] = a_new
-
-        metadata.to_csv(self._results["metadata_file"], sep='\t', index=False)
-
-        return runtime
-
-
 class GatherConfoundsInputSpec(BaseInterfaceInputSpec):
     signals = File(exists=True, desc='input signals')
     dvars = File(exists=True, desc='file containing DVARS')
     std_dvars = File(exists=True, desc='file containing standardized DVARS')
     fd = File(exists=True, desc='input framewise displacement')
     rmsd = File(exists=True, desc='input RMS framewise displacement')
-    tcompcor = File(exists=True, desc='input tCompCorr')
-    acompcor = File(exists=True, desc='input aCompCorr')
-    cos_basis = File(exists=True, desc='input cosine basis')
     motion = File(exists=True, desc='input motion parameters')
-    aroma = File(exists=True, desc='input ICA-AROMA')
 
 
 class GatherConfoundsOutputSpec(TraitedSpec):
@@ -227,11 +99,7 @@ class GatherConfounds(SimpleInterface):
             std_dvars=self.inputs.std_dvars,
             fdisp=self.inputs.fd,
             rmsd=self.inputs.rmsd,
-            tcompcor=self.inputs.tcompcor,
-            acompcor=self.inputs.acompcor,
-            cos_basis=self.inputs.cos_basis,
             motion=self.inputs.motion,
-            aroma=self.inputs.aroma,
             newpath=runtime.cwd,
         )
         self._results['confounds_file'] = combined_out
@@ -239,49 +107,9 @@ class GatherConfounds(SimpleInterface):
         return runtime
 
 
-class ICAConfoundsInputSpec(BaseInterfaceInputSpec):
-    in_directory = Directory(mandatory=True, desc='directory where ICA derivatives are found')
-    skip_vols = traits.Int(desc='number of non steady state volumes identified')
-    err_on_aroma_warn = traits.Bool(False, usedefault=True, desc='raise error if aroma fails')
-
-
-class ICAConfoundsOutputSpec(TraitedSpec):
-    aroma_confounds = traits.Either(
-        None,
-        File(exists=True, desc='output confounds file extracted from ICA-AROMA'))
-    aroma_noise_ics = File(exists=True, desc='ICA-AROMA noise components')
-    melodic_mix = File(exists=True, desc='melodic mix file')
-    aroma_metadata = File(exists=True, desc='tabulated ICA-AROMA metadata')
-
-
-class ICAConfounds(SimpleInterface):
-    """Extract confounds from ICA-AROMA result directory
-    """
-    input_spec = ICAConfoundsInputSpec
-    output_spec = ICAConfoundsOutputSpec
-
-    def _run_interface(self, runtime):
-        (aroma_confounds,
-         motion_ics_out,
-         melodic_mix_out,
-         aroma_metadata) = _get_ica_confounds(self.inputs.in_directory,
-                                              self.inputs.skip_vols,
-                                              newpath=runtime.cwd)
-
-        if self.inputs.err_on_aroma_warn and aroma_confounds is None:
-            raise RuntimeError('ICA-AROMA failed')
-
-        aroma_confounds = self._results['aroma_confounds'] = aroma_confounds
-
-        self._results['aroma_noise_ics'] = motion_ics_out
-        self._results['melodic_mix'] = melodic_mix_out
-        self._results['aroma_metadata'] = aroma_metadata
-        return runtime
-
 
 def _gather_confounds(signals=None, dvars=None, std_dvars=None, fdisp=None,
-                      rmsd=None, tcompcor=None, acompcor=None, cos_basis=None,
-                      motion=None, aroma=None, newpath=None):
+                      rmsd=None, motion=None, newpath=None):
     r"""
     Load confounds from the filenames, concatenate together horizontally
     and save new file.
@@ -332,11 +160,7 @@ def _gather_confounds(signals=None, dvars=None, std_dvars=None, fdisp=None,
                            (dvars, 'DVARS'),
                            (fdisp, 'Framewise displacement'),
                            (rmsd, 'Framewise displacement (RMS)'),
-                           (tcompcor, 'tCompCor'),
-                           (acompcor, 'aCompCor'),
-                           (cos_basis, 'Cosine basis'),
-                           (motion, 'Motion parameters'),
-                           (aroma, 'ICA-AROMA')):
+                           (motion, 'Motion parameters')):
         if confound is not None and isdefined(confound):
             confounds_list.append(name)
             if os.path.exists(confound) and os.stat(confound).st_size > 0:
@@ -362,81 +186,9 @@ def _gather_confounds(signals=None, dvars=None, std_dvars=None, fdisp=None,
     return combined_out, confounds_list
 
 
-def _get_ica_confounds(ica_out_dir, skip_vols, newpath=None):
-    if newpath is None:
-        newpath = os.getcwd()
-
-    # load the txt files from ICA-AROMA
-    melodic_mix = os.path.join(ica_out_dir, 'melodic.ica/melodic_mix')
-    motion_ics = os.path.join(ica_out_dir, 'classified_motion_ICs.txt')
-    aroma_metadata = os.path.join(ica_out_dir, 'classification_overview.txt')
-    aroma_icstats = os.path.join(ica_out_dir, 'melodic.ica/melodic_ICstats')
-
-    # Change names of motion_ics and melodic_mix for output
-    melodic_mix_out = os.path.join(newpath, 'MELODICmix.tsv')
-    motion_ics_out = os.path.join(newpath, 'AROMAnoiseICs.csv')
-    aroma_metadata_out = os.path.join(newpath, 'classification_overview.tsv')
-
-    # copy metion_ics file to derivatives name
-    shutil.copyfile(motion_ics, motion_ics_out)
-
-    # -1 since python lists start at index 0
-    motion_ic_indices = np.loadtxt(motion_ics, dtype=int, delimiter=',', ndmin=1) - 1
-    melodic_mix_arr = np.loadtxt(melodic_mix, ndmin=2)
-
-    # pad melodic_mix_arr with rows of zeros corresponding to number non steadystate volumes
-    if skip_vols > 0:
-        zeros = np.zeros([skip_vols, melodic_mix_arr.shape[1]])
-        melodic_mix_arr = np.vstack([zeros, melodic_mix_arr])
-
-    # save melodic_mix_arr
-    np.savetxt(melodic_mix_out, melodic_mix_arr, delimiter='\t')
-
-    # process the metadata so that the IC column entries match the BIDS name of
-    # the regressor
-    aroma_metadata = pd.read_csv(aroma_metadata, sep='\t')
-    aroma_metadata['IC'] = [
-        'aroma_motion_{}'.format(name) for name in aroma_metadata['IC']]
-    aroma_metadata.columns = [
-        re.sub(r'[ |\-|\/]', '_', c) for c in aroma_metadata.columns]
-
-    # Add variance statistics to metadata
-    aroma_icstats = pd.read_csv(
-        aroma_icstats, header=None, sep='  ')[[0, 1]] / 100
-    aroma_icstats.columns = [
-        'model_variance_explained', 'total_variance_explained']
-    aroma_metadata = pd.concat([aroma_metadata, aroma_icstats], axis=1)
-
-    aroma_metadata.to_csv(aroma_metadata_out, sep='\t', index=False)
-
-    # Return dummy list of ones if no noise components were found
-    if motion_ic_indices.size == 0:
-        LOGGER.warning('No noise components were classified')
-        return None, motion_ics_out, melodic_mix_out, aroma_metadata_out
-
-    # the "good" ics, (e.g., not motion related)
-    good_ic_arr = np.delete(melodic_mix_arr, motion_ic_indices, 1).T
-
-    # return dummy lists of zeros if no signal components were found
-    if good_ic_arr.size == 0:
-        LOGGER.warning('No signal components were classified')
-        return None, motion_ics_out, melodic_mix_out, aroma_metadata_out
-
-    # transpose melodic_mix_arr so x refers to the correct dimension
-    aggr_confounds = np.asarray([melodic_mix_arr.T[x] for x in motion_ic_indices])
-
-    # add one to motion_ic_indices to match melodic report.
-    aroma_confounds = os.path.join(newpath, "AROMAAggrCompAROMAConfounds.tsv")
-    pd.DataFrame(aggr_confounds.T,
-                 columns=['aroma_motion_%02d' % (x + 1) for x in motion_ic_indices]).to_csv(
-        aroma_confounds, sep="\t", index=None)
-
-    return aroma_confounds, motion_ics_out, melodic_mix_out, aroma_metadata_out
-
-
-class FMRISummaryInputSpec(BaseInterfaceInputSpec):
+class PETSummaryInputSpec(BaseInterfaceInputSpec):
     in_func = File(exists=True, mandatory=True,
-                   desc='input BOLD time-series (4D file) or dense timeseries CIFTI')
+                   desc='input dynamic PET data (4D file) or dense timeseries CIFTI')
     in_mask = File(exists=True,
                    desc='3D brain mask')
     in_segm = File(exists=True, desc='resampled segmentation')
@@ -454,23 +206,23 @@ class FMRISummaryInputSpec(BaseInterfaceInputSpec):
                        desc='the repetition time')
 
 
-class FMRISummaryOutputSpec(TraitedSpec):
+class PETSummaryOutputSpec(TraitedSpec):
     out_file = File(exists=True, desc='written file path')
 
 
-class FMRISummary(SimpleInterface):
+class PETSummary(SimpleInterface):
     """
     Copy the x-form matrices from `hdr_file` to `out_file`.
     """
-    input_spec = FMRISummaryInputSpec
-    output_spec = FMRISummaryOutputSpec
+    input_spec = PETSummaryInputSpec
+    output_spec = PETSummaryOutputSpec
 
     def _run_interface(self, runtime):
         from niworkflows.viz.plots import fMRIPlot
 
         self._results['out_file'] = fname_presuffix(
             self.inputs.in_func,
-            suffix='_fmriplot.svg',
+            suffix='_petplot.svg',
             use_ext=False,
             newpath=runtime.cwd)
 
