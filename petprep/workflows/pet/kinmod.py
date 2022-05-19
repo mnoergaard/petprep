70767f33a623225cfe0d92927273c994fff28805
Martin Norgaard <martin.noergaard@nru.dk>
2022-05-19 12:08:22 -0700

    ENH: add kinmod wf

--- petprep/workflows/pet/kinmod.py
+++ petprep/workflows/pet/kinmod.py
@@ -9,4 +9,44 @@ Created on Thu May  5 17:45:50 2022
 from nipype.interfaces.freesurfer import petsurfer as ps
 
 def init_pet_kinmod_wf():
-    
+    """
+    Build a workflow to estimate head-motion parameters.
+
+    This workflow estimates the motion parameters to perform
+    :abbr:`HMC (head motion correction)` over the input
+    :abbr:`PET (Positron Emission Tomography)` image.
+
+    Workflow Graph
+        .. workflow::
+            :graph2use: orig
+            :simple_form: yes
+
+            from petprep.workflows.pet import init_pet_hmc_wf
+            wf = init_pet_hmc_wf(
+                mem_gb=3,
+                omp_nthreads=1)
+
+    Parameters
+    ----------
+    mem_gb : :obj:`float`
+        Size of PET file in GB
+    omp_nthreads : :obj:`int`
+        Maximum number of threads an individual process may use
+    name : :obj:`str`
+        Name of workflow (default: ``pet_hmc_wf``)
+
+    Inputs
+    ------
+    pet_file
+        PET NIfTI file
+
+    Outputs
+    -------
+    xforms
+        ITKTransform file aligning each volume to ``ref_image``
+    movpar_file
+        MCFLIRT motion parameters, normalized to SPM format (X, Y, Z, Rx, Ry, Rz)
+    rms_file
+        Framewise displacement as measured by ``fsl_motion_outliers`` [Jenkinson2002]_.
+
+    """
